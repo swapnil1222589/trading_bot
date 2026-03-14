f86e83ce80fefb985e556c8471b42d773be533b6
Swapnil Ghuge <swapnilghuge03782@gmail.com>
2026-03-14 14:30:56 +0530

    Create logging_config.py

---
+++ bot/bot/bot/bot/logging_config.py
@@ -0,0 +1,14 @@
+import logging
+
+def setup_logger():
+
+    logging.basicConfig(
+
+        filename="bot.log",
+
+        level=logging.INFO,
+
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    return logging.getLogger()
